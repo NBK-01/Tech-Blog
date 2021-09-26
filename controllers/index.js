b5325e824febd77a686cc2ae7448812568ead631
unknown <nayefkanaan03@gmail.com>
2021-09-26 23:21:02 +0300

    Add routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const homeRoutes = require('./view/home')
+const homeRoutes = require("./view/home");
+const authRoutes = require("./view/auth");
+const dashboardRoutes = require("./view/dashboard");
+const blogRoutes = require("./view/blog");
 
-
+router.use("/", homeRoutes);
+router.use("/", authRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/blogs", blogRoutes);
 
 module.exports = router;
