b933a24046837d642dc80ad3606c5813509ce507
unknown <nayefkanaan03@gmail.com>
2021-09-26 22:58:25 +0300

    Adding routes and server.js editing
    :wq
    :q

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-var exphbs = require('express-handlebars');
+// var exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.engine(".hbs", exphbs({extname: ".hbs"}));
+// app.engine(".hbs", exphbs({extname: ".hbs"}));
 app.use(express.json());
 app.use(express.urlencoded)({extended: true});
 app.use(express.static(path.join(__dirname,  'public')));
