788eb8fce673c46cb998c883440b46ff0198d503
unknown <nayefkanaan03@gmail.com>
2021-09-26 23:04:32 +0300

    wip

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./view/home')
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
-// var exphbs = require('express-handlebars');
+//const exphbs = require('express-handlebars');
+const routes = require("./controllers")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 // app.engine(".hbs", exphbs({extname: ".hbs"}));
+app.set("view engine", ".hbs");
 app.use(express.json());
 app.use(express.urlencoded)({extended: true});
 app.use(express.static(path.join(__dirname,  'public')));
